5343346b4ca948dcf5c3bcab914661472d77657f
Jean da Rocha Vertuoso <jean_b123@hotmail.com>
2024-06-29 14:27:47 -0300

    Domain Model

---
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -0,0 +1,64 @@
+package com.devsuperior.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_belonging")
+public class Belonging {
+    
+    private Integer position;
+    
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+
+    public Belonging() {
+    }
+
+    public Belonging(Game game, GameList list, Integer position) {
+        this.position = position;
+        id.setGame(game);
+        id.setList(list);
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public BelongingPK getId() {
+        return id;
+    }
+
+    public void setId(BelongingPK id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Belonging other = (Belonging) obj;
+        return Objects.equals(this.id, other.id);
+    }
+}
